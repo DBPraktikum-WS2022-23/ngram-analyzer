2be22760fe27688824793acb742887ce545c40eb
codaubianca <codau_bianca@yahoo.com>
2022-12-13 22:18:55 +0100

    combined all db init code and structure into db_connection

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import configparser
+from typing import Dict, Any
+from src.database_connection import NgramDBBuilder, NgramDB
+
 def main():
-    print("")
+    # DB init
+    config = configparser.ConfigParser()
+    config.read('settings/config.ini')
+
+    # convert list of tuples to dict
+    connection_settings: Dict[str, str] = {}
+    if config.has_section('database'):
+        # TODO upon connection. prompt user to input username, password and dbname and store them in config.ini
+        params = config.items('database')
+        for key, value in params:
+            if key == 'schema':
+                connection_settings['options'] = f'-c search_path={value}'
+            else:
+                connection_settings[key] = value
+    
+    # Create DB, add tables, open connection:
+    ngram_db: NgramDB = NgramDBBuilder(connection_settings).connect_to_ngram_db()
+    print('Opened connection')
+    #
+    # Work with the database here. For instance:
+    result = ngram_db.execute('SELECT version()')
+    #
+    print(f'PostgreSQL database version: {result}')
+
+    del ngram_db
+    print('Closed connection')
 
 
 if __name__ == '__main__':
--- src/database_connection.py
+++ src/database_connection.py
@@ -1,43 +1,106 @@
-import psycopg
-from psycopg import sql
-from typing import List, Any
+from __future__ import annotations
 
-from src.data_access import NgramDB
+from dataclasses import dataclass
+from types import TracebackType
+from typing import Dict, List, Optional, Type, Tuple, Any
 
-class DatabaseConnection:
-    def __init(self):
+from psycopg import sql, OperationalError, Connection
+
+@dataclass
+class Ngram:
+    """ TODO """
+
+    def __init__(self):
         pass
+        #TODO
 
-    def __create_database(name: str = 'ngram_db') -> None:
-        con = psycopg.connect(
-                            user='postgres',
-                            host='localhost',
-                            password='1234',
-                            autocommit=True
-                            )
 
-        with con.cursor() as cur:
-            # cur.execute(sql.SQL("CREATE DATABASE {};").format(
-            #         sql.Identifier('test'))
-            #     )
-            cur.execute(f"SELECT 'CREATE DATABASE {name}' \
-                        WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname='{name}');")
-            cmd: List[Any] = cur.fetchall()
-            if cmd == []:
-                return
-            cur.execute(cmd[0][0])
+class NgramDB:
+    """ A wrapper around psycopg classes."""
 
-        con.close()
+    def __init__(self, connection_settings: Dict[Any, str]) -> None:
+        self.__connection = Connection.connect(
+                                                host=connection_settings['host'],
+                                                port=connection_settings['port'],
+                                                user=connection_settings['user'],
+                                                password=connection_settings['password'],
+                                                dbname=connection_settings['dbname'],
+                                                autocommit=True
+                                              )
 
-    def connect_to_db(self, connection_string: str = 'host=localhost \
-                                                port=5432 \
-                                                dbname=ngram_db \
-                                                connect_timeout=10', 
-                        schema:str ='public'
-                        ) -> NgramDB:
-        self.__create_database() # if not exists
-        database: NgramDB = NgramDB(connection_string, schema)
+        self.__create_relations_if_not_exists()
+
+    def __del__(self) -> None:
+        if self.__connection and not self.__connection.closed:
+            self.__connection.close()
+
+    def __enter__(self) -> NgramDB:
+        return self
+
+    @staticmethod
+    def __get_sql_cmds(path: str) -> List[str]:
+        with open(path, "r") as f:
+            cmds = [i.strip() for i in f.read().split(";")]
+        return cmds
+
+    def __create_relations_if_not_exists(self) -> None:
+        # TODO: check if tables are read properly
+        cmds = self.__get_sql_cmds("./src/sql-scripts/db_tables.sql")
+
+        with self.__connection.cursor() as cursor:
+            for cmd in cmds:
+                cursor.execute(cmd)
+
+    def execute(self, query: str, values: Tuple[Any, ...] = (),
+                return_result: bool = True) -> List[Tuple[Any, ...]]:
+        """ Executes the given query and returns the result if desired.
+        Requesting a result on queries that don't return anything causes
+        an Exception. """
+
+        with self.__connection.cursor() as cur:
+            cur.execute(query, values)
+        # TODO add flag to return just fetchone
+            if return_result:
+                return cur.fetchall()
+
+            return []
+
+class NgramDBBuilder:
+
+    def __init__(self, connection_settings: Dict[str, str]) -> bool:
+        self.connection_settings = connection_settings
+        # self.connection_string = self.__dict_to_str(connection_settings)
+        #self.schema = schema
+
+    def __create_database(self) -> None:
+        try:
+            con = Connection.connect(
+                                host=self.connection_settings['host'],
+                                port=self.connection_settings['port'],
+                                user=self.connection_settings['user'],
+                                password=self.connection_settings['password'],
+                                autocommit=True
+                                )
+            with con.cursor() as cur:
+                name: str = self.connection_settings['dbname']
+                cur.execute("SELECT 1 \
+                            WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname=%s);", (name, ))
+                cmd: str = cur.fetchall()
+                # if list is empty, table already exists
+                if not cmd:
+                    return
+                cur.execute(f"CREATE DATABASE {name};")
+                return True
+        except OperationalError as er:
+            print('Failed to connect to database. Check connection settings.')
+            return False
+        finally:
+            if con:
+                con.close()
+
+    def connect_to_ngram_db(self) -> Optional[NgramDB]:
+        if not self.__create_database():
+            return None        
+        database: NgramDB = NgramDB(self.connection_settings)
         print("Connection to database established.")
         return database
-
-
