23ff24b7a1ee8a157937355867c9168593a516f2
junchuan <junchuan1996@gmail.com>
2022-12-13 22:52:51 +0100

    db_tables.sql: SQL scripts for creating table word and occurence

---
+++ src/db_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS word(
+id serial,
+str_rep varchar(50) NOT NULL,
+PRIMARY KEY(id));
+
+CREATE TABLE IF NOT EXISTS occurence(
+id serial,
+year int NOT NULL,
+book_count int NOT NULL CHECK(book_count >= 0),
+freq int NOT NULL CHECK(freq >= 0),
+PRIMARY KEY(id, year),
+FOREIGN KEY(id) REFERENCES word(id));
