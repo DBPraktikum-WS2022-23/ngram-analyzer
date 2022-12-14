4dc2f6bccecf238691407d20f87751e6f28ea401
Yang Zhou <yang.zhou23@hotmail.com>
2022-12-14 14:05:45 +0100

    add spark connection and a write function

--- src/database_connection.py
+++ src/database_connection.py
@@ -7,6 +7,8 @@ from typing import Dict, List, Optional, Type, Tuple, Any
 from psycopg import sql, OperationalError, Connection
 from psycopg.errors import DuplicateDatabase
 
+from pyspark.sql import SparkSession, DataFrame
+
 @dataclass
 class Ngram:
     """ TODO """
@@ -28,6 +30,18 @@ class NgramDB:
                                                 dbname=connection_settings['dbname'],
                                                 autocommit=True
                                               )
+
+        # TODO: examples from ilias, need to be changed and moved into config file
+        self.__db_url = 'jdbc:postgresql://localhost:5432/googlengram'
+        self.__properties = {"user": "postgres", "password": "postgres"}
+        self.__spark = SparkSession.builder \
+            .master("local") \
+            .appName("NgramDB") \
+            .config("spark.driver.extraClassPath",
+                    "postgresql-42.2.18.jar") \
+            .config("spark.driver.memory", "4g") \
+            .config("spark.executor.memory", "1g") \
+            .getOrCreate()
 
         self.__create_relations_if_not_exists()
 
@@ -65,6 +79,10 @@ class NgramDB:
                 return cur.fetchall()
 
             return []
+    
+    def write(self, df: DataFrame, table: str) -> None:
+        """ Writes the given DataFrame to the given table by using DataFrame. """
+        df.write.jdbc(self.__db_url, table, mode="append", properties=self.__properties)
 
 class NgramDBBuilder:
 
