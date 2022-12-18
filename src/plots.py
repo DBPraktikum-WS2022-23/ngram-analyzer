e29a9df7b3cb8195573a40874bfdd8c7b2ba0c1b
Franzi <92818644+franziskavonalbedyll@users.noreply.github.com>
2022-12-18 18:25:49 +0100

    feat: working version of plot_word_frequencies

--- src/plots.py
+++ src/plots.py
@@ -1,24 +1,35 @@
 import matplotlib.pyplot as plt
-from typing import Dict
+from typing import List
 from pyspark.sql import SparkSession, DataFrame
 
-#TODO save plots to output folder
-# write a basic function to plot example data
-def plot_example_data() -> None:
-    x = [1, 2, 3, 4, 5]
-    y = [1, 4, 9, 16, 25]
-    plt.plot(x, y)
-    plt.show()
-    # save plot
-    plt.savefig("output/example_plot.png")
-
-# plot the frequency of certain words in certain years
 def plot_word_frequencies(words, years) -> None:
-    spark = SparkSession.builder.master("local[1]")\
-        .appName("test")\
+    """ Plot the frequency of certain words in certain years"""
+    spark = SparkSession \
+        .builder \
+        .appName("Python Spark SQL") \
+        .config("spark.jars", "/resources/postgresql-42.5.1.jar") \
         .getOrCreate()
 
-    df = spark.read.csv("data/test_data.csv", header=True, inferSchema=True)
+    df_word = spark.read \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://localhost:5432/ngram_db") \
+        .option("dbtable", "word") \
+        .option("user", "postgres") \
+        .option("password", "abcd1234") \
+        .option("driver", "org.postgresql.Driver") \
+        .load()
+
+    df_occurence = spark.read \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://localhost:5432/ngram_db") \
+        .option("dbtable", "occurence") \
+        .option("user", "postgres") \
+        .option("password", "abcd1234") \
+        .option("driver", "org.postgresql.Driver") \
+        .load()
+
+    # create a spark dataframe with the words and years
+    string_representations: List[str] = df_word.filter(df_word.str_rep.isin(words)).select("id", "str_rep").distinct().collect()
 
     # plot the frequency of the words in the years into a single line plot
     fig, ax = plt.subplots()
@@ -27,10 +38,10 @@ def plot_word_frequencies(words, years) -> None:
     ax.set_xticks(range(min(years), max(years)+1))
     ax.set_ylabel("frequency")
 
-    for word in words:
-        df_word = df.filter(df.word == word)
-        df_word = df_word.filter(df.year.isin(years))
-        ax.scatter(df_word.select("year").collect(), df_word.select("frequency").collect(), label=word)
+    for row in string_representations:
+        df = df_occurence.filter(df_occurence.id == row.id)
+        df = df.filter(df_occurence.year.isin(years))
+        ax.scatter(df.select("year").collect(), df.select("freq").collect(), label=row.str_rep)
 
     ax.legend()
     plt.show()
