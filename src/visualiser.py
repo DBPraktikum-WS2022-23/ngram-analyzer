235f86f04eb699a594e8ca013d58537dc46b5998
junchuan <junchuan1996@gmail.com>
2023-01-11 00:27:03 +0100

    add TODOs and comments

--- src/visualiser.py
+++ src/visualiser.py
@@ -14,13 +14,12 @@ class Visualiser:
     def __init__(
         self, spark: SparkSession, db_url: str, properties: Dict[str, str]
     ) -> None:
-        """Set uo Word Frequency Object"""
         self.db2df: DatabaseToSparkDF = DatabaseToSparkDF(spark, db_url, properties)
         self.df_word: DataFrame = self.db2df.df_word
         self.df_occurence: DataFrame = self.db2df.df_occurence
 
     def plot_kde(self) -> None:
-        """Plot the frequency of certain words in certain years"""
+        """Plot the Kernel Density Estimation with Gauss-Kernel of a word"""
 
         # TODO: these are inputs from info.py
         bandwidth: float = 0.25
@@ -28,6 +27,7 @@ class Visualiser:
         years = [1832, 1854, 1863, 1868, 1887, 1888, 1890]
         freqs = [1, 1, 1, 1, 3, 2, 1]
 
+        # TODO: set axis and legend
         _, axis = plt.subplots()
         axis.set_title("Kernel Density Estimation with Gauss-Kernel")
         axis.set_xlabel("year")
@@ -46,9 +46,8 @@ class Visualiser:
         axis.legend()
         plt.show()
 
-        # check if the directory output already exists, if not, create it
         if not os.path.exists("output"):
             os.mkdir("output")
-        plt_name: str = f"output/kdeplot_{word}.png"
+        plt_name: str = f"output/kde_plot_{word}.png"
         plt.savefig(plt_name)
         print(f"Saved {plt_name} to output directory")
