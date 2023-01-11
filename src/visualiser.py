62cc7befef54a4a2eb51dba1a5c60159ccdb4d1a
junchuan <ulgzj@student.kit.edu>
2023-01-12 00:02:16 +0100

    add plot_scatter_all in visualiser

--- src/visualiser.py
+++ src/visualiser.py
@@ -18,6 +18,45 @@ class Visualiser:
         self.df_word: DataFrame = self.db2df.df_word
         self.df_occurence: DataFrame = self.db2df.df_occurence
 
+    def plot_scatter_all(self, df: DataFrame) -> None:
+        """Plot the frequency of all words in certain years"""
+        years = list(range(1800, 2000))
+
+        _, axis = plt.subplots()
+        axis.set_title("Frequency of words in years")
+        axis.set_xlabel("year")
+        axis.set_xticks(range(min(years), max(years) + 1))
+        axis.set_ylabel("frequency")
+
+        for row in df.collect():
+            axis.scatter(
+                years,
+                row.select([c for c in df.columns if c in str(list(range(1800, 2000)))]).collect(),
+                label=row.select("str_rep", "type").collect(),
+            )
+            # TODO: Regression, get a, b from Aufgabe 1
+            # TODO: How to differ the plots from different 1-Gramm
+            """# Fit linear regression via least squares with numpy.polyfit
+            # It returns an slope (b) and intercept (a)
+            # deg=1 means linear fit (i.e. polynomial of degree 1)
+            b, a = np.polyfit(x, y, deg=1)
+
+            # Create sequence of 100 numbers from 0 to 100 
+            xseq = np.linspace(0, 10, num=100)
+
+            # Plot regression line
+            ax.plot(xseq, a + b * xseq, color="k", lw=2.5);"""
+
+        axis.legend()
+        plt.show()
+
+        # check if the directory output already exists, if not, create it
+        if not os.path.exists("output"):
+            os.mkdir("output")
+        plt_name: str = f"output/scatter_plot_all.png"
+        plt.savefig(plt_name)
+        print(f"Saved {plt_name} to output directory")
+
     def plot_kde(self) -> None:
         """Plot the Kernel Density Estimation with Gauss-Kernel of a word"""
 
