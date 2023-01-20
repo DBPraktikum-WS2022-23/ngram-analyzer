705e773727ee5abfdb5d77ef2c2356ecd4200c65
unjlq <unjlq@student.kit.edu>
2023-01-20 15:58:24 +0100

    Plugins

--- src/cli.py
+++ src/cli.py
@@ -5,7 +5,7 @@ import sys
 from typing import Dict, Optional
 
 from src.config_converter import ConfigConverter, ConfigCreator
-from src.controller import SparkController
+from src.controller import SparkController, PluginController
 from src.database_creation import NgramDBBuilder
 from src.shell import Prompt
 
@@ -17,7 +17,7 @@ class Cli:
 
     def __init__(self) -> None:
         self.conn_settings: Dict[str, str] = {}
-        self.spark = None
+        self.spark_controller = None
 
     def __exit(self) -> None:
         if self.spark:
@@ -32,12 +32,32 @@ class Cli:
         sys.exit(f"Error: {message}.")
 
     def run_cli(self) -> None:
-        """Define and analyze the argument structure."""
+        """Execute the program according to the passed arguments."""
+
+        args: dict = self.__parse_args()
+
+        # if no arguments are supplied or user asks for shell, start shell
+        if (len(sys.argv) == 1) or args["shell"]:
+            Prompt().cmdloop()
+            return
+
+        if args["create_db"]:
+            self.__create_db(args)
+            return
+
+        if args["transfer"]:
+            self.__transfer_data(args)
+            return
+
+
+        self.__exit()
+
+    def __parse_args(self) -> dict:
+        """Parse the arguments given to the program. Returns a dictionary with the arguments."""
         psr = argparse.ArgumentParser(
-            prog="main.py",
-            description="A program to store ngrams in a database and analyse "
-            "them. To start interactive shell call program without parameters",
-            epilog="Enjoy using our program!",
+        prog="main.py",
+        description="A program to store ngrams in a database and analyse "
+        "them. To start interactive shell call program without parameters"
         )
 
         psr.add_argument(
@@ -98,87 +118,82 @@ class Cli:
             help="Name for the database",
         )
 
-        args = vars(psr.parse_args())
+        psr.add_argument(
+            "-pp",
+            "--plugin_path",
+            help="Path to the plugin folder, default: plugins",
+        )
 
-        # if no arguments are supplied or user asks for shell, start shell
-        if (len(sys.argv) == 1) or args["shell"]:
-            Prompt().cmdloop()
-            return
+        return vars(psr.parse_args())
 
-        if args["create_db"]:
-            if (
+    def __create_db(self, args) -> None:
+        """Creates the database incl. its relations, if not already existing."""
+        self.__set_config_information(args)
+        NgramDBBuilder(self.conn_settings).create_ngram_db()  # create database
+
+        return
+
+
+    def __transfer_data(self, args) -> None:
+        """Reads raw data from supplied file or folder into database"""
+        self.__set_config_information(args)
+
+        # no path has been given so use the default path from the sample configuration
+        if args["data_path"] is not None:
+            path = os.path.abspath(args["data_path"])
+            if not os.path.exists(path):
+                self.__exit_error(f"data path '{path}' does not exist")
+        else:
+            path = "./data"
+
+        # check if db with db_name exists (necessary because a new configuration can be generated above)
+        db_builder = NgramDBBuilder(self.conn_settings)
+        if not db_builder.exists_db():
+            self.__exit_error(f"Database {args['dbname']} does not exist. Please use the --db_create option")
+
+        data_files = []  # list of files to process
+
+        # get files from path
+        if os.path.isfile(path):
+            data_files.append(path)  # single file
+        elif os.path.isdir(path):  # handle directory
+            for cur_path, _, files in os.walk(path):
+                for file in files:
+                    data_files.append(os.path.join(cur_path, file))
+
+        spark_controller: SparkController = SparkController(self.conn_settings)
+        spark_controller.transfer(data_files)
+
+        return
+
+    def __check_for_config_information(self, args) -> None:
+        if (
                 args["username"] is None
                 or args["password"] is None
                 or args["dbname"] is None
-            ) and args["config_path"] is None:
-                self.__exit_error(
-                    "Missing arguments for database creation. "
-                    "Please rerun the program providing a user, a password and a dbname"
-                    " or a config file"
-                )
-            if args["config_path"] is not None:
-                conn_settings = ConfigConverter(
-                    args["config_path"]
-                ).get_conn_settings()
-            else:
-                config_path = ConfigCreator(
-                    args["username"], args["password"], args["dbname"]
-                ).generate_new_config()
-                conn_settings = ConfigConverter(
-                    config_path
-                ).get_conn_settings()
-            NgramDBBuilder(conn_settings).create_ngram_db()  # create database
+        ) and args["config_path"] is None:
+            self.__exit_error(
+                "Missing arguments. "
+                "Please rerun the program providing a user, a password and a dbname"
+                " or a config file"
+            )
 
-        if args["transfer"]:
-            if (
-                args["username"] is None
-                or args["password"] is None
-                or args["dbname"] is None
-            ) and (args["config_path"] is None):
-                self.__exit_error(
-                    "Missing arguments for transfer. "
-                    "Please rerun the program providing a user, a password and a dbname"
-                    " or a config file"
-                )
+        return
 
-            # use SparkController to transfer files
-            if args["config_path"] is not None:
-                converter = ConfigConverter(
-                    args["config_path"]
-                )             
-            else:
-                config_path = ConfigCreator(
-                    args["username"], args["password"], args["dbname"]
-                ).generate_new_config()
-                converter = ConfigConverter(config_path)
+    def __set_config_information(self, args) -> None:
+        """ Set the config information from the arguments """
+        self.__check_for_config_information(args)
 
-            conn_settings = converter.get_conn_settings()
-            default_path = converter.get_data_path()   
+        if args["config_path"] is not None:
+            conn_settings = ConfigConverter(
+                args["config_path"]
+            ).get_conn_settings()
+        else:
+            config_path = ConfigCreator(
+                args["username"], args["password"], args["dbname"]
+            ).generate_new_config()
+            conn_settings = ConfigConverter(
+                config_path
+            ).get_conn_settings()
 
-            # no path has been given so use the default path from the sample configuration
-            if args["data_path"] is not None:
-                path = os.path.abspath(args["data_path"])
-                if not os.path.exists(path):
-                    self.__exit_error(f"data path '{path}' does not exist")
-            else:
-                path = default_path
-
-            # check if db with db_name exists (necessary because a new configuration can be generated above)
-            db_builder = NgramDBBuilder(conn_settings)
-            if not db_builder.exists_db():
-                self.__exit_error(f"Database {args['dbname']} does not exist. Please use the --db_create option")
-
-            data_files = []  # list of files to process
-
-            # get files from path
-            if os.path.isfile(path):
-                data_files.append(path)  # single file
-            elif os.path.isdir(path):  # handle directory
-                for cur_path, _, files in os.walk(path):
-                    for file in files:
-                        data_files.append(os.path.join(cur_path, file))
-
-            spark_controller: SparkController = SparkController(conn_settings)
-            spark_controller.transfer(data_files)
-
-        self.__exit()
+        self.conn_settings = conn_settings
