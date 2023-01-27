8fcb3badcdc6068f414e49c372da3d7624e937f2
codaubianca <codau_bianca@yahoo.com>
2023-01-27 16:35:13 +0100

    Fixed lof bug --> knns should contain the current ts

--- src/plugins/LOFPlugin.py
+++ src/plugins/LOFPlugin.py
@@ -15,7 +15,7 @@ class ExternalOutlierDetector(ABC):
         pass
 
 class LOFOutlierDetector(ExternalOutlierDetector):
-    def __init__(self, k: int, delta: float, data: List[List[int]]) -> None:
+    def __init__(self, k: int, delta: float) -> None:
         super().__init__()
         self.__k = k
         self.__delta = delta
@@ -29,6 +29,7 @@ class LOFOutlierDetector(ExternalOutlierDetector):
         return 1 / mean_reach_distance 
 
     def __get_knns(self, time_series: List[int], time_series_list: List[List[int]]) -> List[List[int]]:
+        # time_series_list should not contain time_series
         distances = []
         for current_ts in time_series_list:
             current_distance = distance.euclidean(time_series, current_ts)
@@ -39,30 +40,27 @@ class LOFOutlierDetector(ExternalOutlierDetector):
 
     def detect_outliers(self, time_series_list: List[List[int]]) -> List[int]:
         lof_list = []
-        for time_series in time_series_list:
+        for id, time_series in enumerate(time_series_list):
             lof = 0
-            knns = self.__get_knns(time_series, time_series_list)
+
+            possible_knns = time_series_list.copy()
+            possible_knns.pop(id)
+            knns = self.__get_knns(time_series, possible_knns)
+
+            lrd_x = self.__local_reachability_density(time_series, knns)
             for neighbor in knns:
-                lof += self.__local_reachability_density(neighbor, knns) / self.__local_reachability_density(time_series, knns)
-            lof = lof / self.__k
+                lrd_n = self.__local_reachability_density(neighbor, knns)
+                lof += lrd_n / lrd_x
+            lof = lof / len(knns)
             lof_list.append(lof)
-        
+
         lof_index_list = []
 
         for i in range(len(lof_list)):
-            print("Lof for ts ", i, '\n', lof_list[i])
             if lof_list[i] > self.__delta:
                 lof_index_list.append(i)
         
         return lof_index_list
-
-if __name__ == "__main__":
-    ts = [2,3,10,0]
-    ts_list = [[2,3,1,0], [1,3,4,2], [3,4,5,0], [2,10,10,10], [3,4,5,10]]
-    lof = LOFOutlierDetector(2, 0.1, [])
-    #knns = lof.get_knns(ts, ts_list)
-    #print(ts, '\n', ts_list, '\n', knns)
-    print(ts_list, '\n', lof.detect_outliers(ts_list))
 
 class LOFPlugin(BasePlugin):
     def __init__(self, **kwargs) -> None:
